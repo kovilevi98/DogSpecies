ba11c8fd5b19a9af50f213875f041f6bc05094c6
Levi <kovilevi98@gmail.com>
2022-03-27 19:26:43 +0200

    + add files for main ui

--- app/src/main/java/hu/bme/aut/dogspecies/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/dogspecies/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package hu.bme.aut.dogspecies
+package hu.bme.aut.dogspecies.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.bme.aut.dogspecies.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
